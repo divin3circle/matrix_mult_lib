5f4cf683540747b7732cfe6bb588444c9173c940
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-06-01 15:03:48 +0300

    added cmakelist file and updated conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,39 @@
 
-from conans import ConanFile, CMake
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
-class MatrixMultLibConan(ConanFile):
+
+class helloRecipe(ConanFile):
     name = "matrix_mult_lib"
-    version = "0.1.0"
+    version = "1.0"
+
+    # Optional metadata
     license = "MIT"
-    author = "divin3circle"
+    author = "divin3circle sylusabel1@gmail.com"
     url = "https://github.com/divin3circle/matrix_mult_lib"
-    description = "A simple C library for 3x3 matrix multiplication"
+    description = "A simple matrix multiplication library."
+    topics = ("math", "matrices", "multiplication")
+
+    # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
-    exports_sources = "src/*", "include/*", "CMakeLists.txt"
-    generators = "cmake"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*", "include/*"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -18,9 +41,8 @@ class MatrixMultLibConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include", src="include")
-        self.copy("*.a", dst="lib", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
+        cmake = CMake(self)
+        cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["matrix_mult"]
+        self.cpp_info.libs = ["hello"]
